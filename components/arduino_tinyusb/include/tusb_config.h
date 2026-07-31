43b9228c558a43e5ad0fb865e09db57b4763080b
Marek Knosala <44412229+rvbc1@users.noreply.github.com>
2026-07-31 20:15:47 +0200

    Optimize TinyUSB for AudioFlex CDC

--- components/arduino_tinyusb/include/tusb_config.h
+++ components/arduino_tinyusb/include/tusb_config.h
@@ -195,23 +195,23 @@ extern "C" {
 /*  HOST CONFIGURATION  */
 /*                      */
 
-#define CFG_TUH_ENABLED             CFG_TUD_ENABLED
+#define CFG_TUH_ENABLED             0
 #define CFG_TUSB_RHPORT1_MODE       OPT_MODE_HOST
 #define BOARD_TUH_RHPORT            1
 #define BOARD_TUH_MAX_SPEED         CFG_TUD_MAX_SPEED
 #define CFG_TUH_ENUMERATION_BUFSIZE 256
 
-#define CFG_TUH_HUB                 2 // number of supported hubs
-#define CFG_TUH_CDC                 1 // CDC ACM
-#define CFG_TUH_CDC_FTDI            1 // FTDI Serial.  FTDI is not part of CDC class, only to re-use CDC driver API
-#define CFG_TUH_CDC_CP210X          1 // CP210x Serial. CP210X is not part of CDC class, only to re-use CDC driver API
-#define CFG_TUH_CDC_CH34X           1 // CH340 or CH341 Serial. CH34X is not part of CDC class, only to re-use CDC driver API
-#define CFG_TUH_HID                 1 // typical keyboard + mouse device can have 3-4 HID interfaces
-#define CFG_TUH_MSC                 1
-#define CFG_TUH_MIDI                1 // MIDI host driver; midi_host.c is already compiled, this enables it
+#define CFG_TUH_HUB                 0
+#define CFG_TUH_CDC                 0
+#define CFG_TUH_CDC_FTDI            0
+#define CFG_TUH_CDC_CP210X          0
+#define CFG_TUH_CDC_CH34X           0
+#define CFG_TUH_HID                 0
+#define CFG_TUH_MSC                 0
+#define CFG_TUH_MIDI                0
 //#define CFG_TUH_VENDOR              3
 
-#define CFG_TUH_DEVICE_MAX          (3*CFG_TUH_HUB + 1)
+#define CFG_TUH_DEVICE_MAX          1
 
 //------------- HID -------------//
 #define CFG_TUH_HID_EPIN_BUFSIZE    64
